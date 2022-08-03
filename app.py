2728614500a37fc9b8f3d9a58650c9a7af987394
swati banka <83939447+swatibanka@users.noreply.github.com>
2022-08-03 12:44:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,16 +13,25 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/predict',methods=['POST'])
+
+#@app.route('/hello', methods=['GET'])
+
+#def get():
+    #return "Hello world"
+
+@app.route('/predict',methods=['GET','POST'])
+
 def predict():
+    if request.method == 'POST':
+        int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
+        features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
+        prediction = model.predict(features)  # features Must be in the form [[a, b]]
 
-    int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
-    features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
-    prediction = model.predict(features)  # features Must be in the form [[a, b]]
+        output = round(prediction[0], 2)
 
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Rate for house is {}'.format(output))
+        return render_template('index.html', prediction_text='Rate for house is {}'.format(output))
+    else:
+        return render_template('index.html', prediction_text='Rate for house is {}'.format(output))
 
 if __name__ == "__main__":
     app.run()
